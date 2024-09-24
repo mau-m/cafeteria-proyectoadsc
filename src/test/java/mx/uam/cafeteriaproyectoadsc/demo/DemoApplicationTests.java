36579d37c6995c6e98e48c439e78d4ee1a79ca04
Mauricio <mirandabmau@gmail.com>
2024-09-23 23:19:42 -0600

    Creacion del proyecto

---
+++ src/test/java/mx/uam/cafeteriaproyectoadsc/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uam.cafeteriaproyectoadsc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
